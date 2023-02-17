217a3196048e213ecdd306767b52a7e35a3c4b11
santoshdahal2016 <dahalsantosh2018@gmail.com>
2023-02-17 21:04:52 +0545

    model building in progress

---
+++ Entity_extraction_using_BERT/src/model.py
@@ -0,0 +1,54 @@
+import config 
+import torch 
+import transformers
+from torch.nn import nn
+
+
+def loss_fn(output , target , mask , num_labels):
+
+    lfn = nn.CrossEntropyLoss()
+
+    active_loss  = mask.view(-1) == 1
+    active_logits =  output.view(-1 , num_labels)
+
+    active_labels = torch.where(
+        active_loss,
+        target.view(-1),
+        torch.tensor( lfn.ignore_index ).type_as(target)
+    )
+
+    loss = lfn(active_logits, active_labels)
+    return loss
+
+
+
+class EntityModel(nn.Module):
+
+    def __init__(self , num_tag , num_pos) -> None:
+        super(EntityModel, self).__init__()
+        self.num_tag = num_tag
+        self.num_pos = num_pos
+        self.bert = transformers.BertModel.from_pretrained(config.BASE_MODEL_PATH)
+        self.dropout1 = nn.Dropout(0.3)
+        self.dropout2 = nn.Dropout(0.3)
+        self.out_tag = nn.Linear(768, self.num_tag)
+        self.out_pos = nn.Linear(768, self.num_pos)
+
+
+    def forward(self, ids, mask , token_type_ids , target_pos , target_tag):
+        out, _ = self.bert(ids, attention_mask = mask , token_type_ids = token_type_ids)
+        bo_tags = self.dropout1(out)
+        bo_pos = self.dropout2(out)
+
+        tag = self.out_tag(bo_tags)
+        pos = self.out_pos(bo_pos)
+
+        loss_tag = loss_fn(tag , target_tag, mask ,self.num_tag )
+        loss_pos = loss_fn(pos , target_pos, mask ,self.num_pos)
+
+        loss = (loss_tag + loss_pos) / 2
+
+        return tag , pos , loss
+
+
+
